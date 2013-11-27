241ff82f5dfdf6d3262ae8f30681cf4b528b952b
drkitty <bumblingkitty@gmail.com>
2013-11-27 02:13:18 -0800

    Add some files

---
+++ plugins.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+
+from config import OWNER
+
+
+def repeat(rest, sender, channel):
+    return rest
+
+
+def elipsis(msg, sender, channel):
+    if msg.strip() == '...':
+        return '...'
+
+def die(msg, sender, channel):
+    if sender.nick == OWNER:
+        exit(0)
+
+
+PLUGINS = [
+    ('die', die),
+    ('repeat', repeat),
+    (False, elipsis),
+]
---
+++ robot.py
@@ -0,0 +1,94 @@
+from __future__ import unicode_literals
+
+import irc.client
+from irc.client import NickMask
+from itertools import ifilter
+from sys import stderr
+
+from config import USERNAME, PASSWORD
+from plugins import PLUGINS
+
+
+class Robot(object):
+    def __init__(self):
+        self.client = irc.client.IRC()
+        self.server = self.client.server()
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        stderr.write('Disconnecting...\n')
+        self.client.disconnect_all()
+        self.server.disconnect()
+
+    def connect(self):
+        def handler_wrapper(func):
+            def wrapped(connection, event):
+                return func(connection, event)
+
+            return wrapped
+
+        stderr.write('Connecting...\n')
+        self.server.connect('irc.freenode.net', 6667, USERNAME)
+        stderr.write('Identifying...\n')
+        self.server.privmsg('NickServ', 'identify {0}'.format(PASSWORD))
+
+        self.client.add_global_handler('privmsg', self.privmsg_handler)
+        self.client.add_global_handler('pubmsg', self.pubmsg_handler)
+
+    def join(self, channel):
+        self.server.join(channel)
+
+    def handle_normal(self, msg, sender, channel=None):
+        plugins = filter(lambda p: not p[0], PLUGINS)
+        for _, func in plugins:
+            response = func(msg, sender, channel)
+            if response:
+                return response
+
+    def handle_mentioned(self, msg, sender, channel=None):
+        splat = msg.split(None, 1)
+        if len(splat) == 1:
+            command = splat[0]
+            rest = ''
+        else:
+            command, rest = splat
+
+        plugin = next(ifilter(lambda t: t[0] == command, PLUGINS), None)
+        if not plugin:
+            return self.handle_normal(msg, sender, channel=None)
+
+        response = plugin[1](rest, sender, channel)
+        if response:
+            return response
+
+    def pubmsg_handler(self, connection, event):
+        msg = event.arguments[0]
+        sender = NickMask(event.source)
+        channel = event.target
+
+        splat = msg.split(':', 1)
+        if len(splat) == 1:
+            response = self.handle_normal(msg, sender, channel)
+        elif len(splat) == 2 and USERNAME in splat[0]:
+            response = self.handle_mentioned(splat[1].lstrip(), sender,
+                                             channel)
+
+        if response:
+            for line in response.split('\n'):
+                self.server.privmsg(channel, sender.nick + ': ' + line)
+
+    def privmsg_handler(self, connection, event):
+        msg = event.arguments[0]
+        sender = NickMask(event.source)
+
+        response = self.handle_mentioned(msg, sender)
+        if response:
+            for line in response.split('\n'):
+                self.server.privmsg(sender.nick, line)
+
+    def go(self):
+        stderr.write('Doing things...\n')
+        self.client.process_forever()
